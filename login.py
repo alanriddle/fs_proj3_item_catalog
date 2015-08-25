f681d7c9c832399266c5ee9ce66bc60955943c69
Alan Riddle <alanriddle@users.noreply.github.com>
2015-08-25 12:15:33 -0400

    Correct spelling of client_secret.json

--- login.py
+++ login.py
@@ -5,7 +5,7 @@ import requests
 
 
 def upgrade_to_credentials(authorization_code):
-    oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+    oauth_flow = flow_from_clientsecrets('client_secret.json', scope='')
     oauth_flow.redirect_uri = 'postmessage'
     credentials = oauth_flow.step2_exchange(authorization_code)
     return credentials
